cc05e5f82f49dc10d0c7a3d2940f1c7ef6d3ca3d
Peter Kováč <pojzinko8@gmail.com>
2023-03-15 20:52:39 +0100

    Test app

--- app/src/main/java/com/example/client/MainActivity.kt
+++ app/src/main/java/com/example/client/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var value = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val incrementButton = findViewById<Button>(R.id.increment_button)
+        val valueTextView = findViewById<TextView>(R.id.value_text_view)
+
+        incrementButton.setOnClickListener {
+            value++
+            valueTextView.text = value.toString()
+        }
     }
 }
