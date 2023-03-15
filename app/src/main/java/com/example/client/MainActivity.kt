947fdb9297751bc966a85d3db4963b31aaa781ec
Peter Kováč <pojzinko8@gmail.com>
2023-03-16 00:21:41 +0100

    Fungujem (dovraciam sa)

--- app/src/main/java/com/example/client/MainActivity.kt
+++ app/src/main/java/com/example/client/MainActivity.kt
@@ -1,25 +1,57 @@
 package com.example.client
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.os.SystemClock
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
 
-    private var value = 0
+    private lateinit var sendMsgButton: Button
+    private lateinit var sendMsgInput: TextInputEditText
+    private lateinit var recvMsgText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val incrementButton = findViewById<Button>(R.id.increment_button)
-        val valueTextView = findViewById<TextView>(R.id.value_text_view)
+        sendMsgButton = findViewById(R.id.send_msg_btn)
+        sendMsgInput = findViewById(R.id.send_msg_input)
+        recvMsgText = findViewById(R.id.recv_msg_text)
 
-        incrementButton.setOnClickListener {
-            value++
-            valueTextView.text = value.toString()
-        }
+        Thread {
+            try {
+                createSocketConnection()
+            } catch (e: Exception) {
+                Log.e("SocketError", "Error occurred: ${e.message}")
+            }
+        }.start()
     }
+}
+
+private fun createSocketConnection() {
+    val destinationAddress = "192.168.100.7"
+    val port = 12345
+    val socket = Socket(destinationAddress, port)
+    val writer = PrintWriter(socket.getOutputStream(), true)
+    val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+
+    for (i in generateSequence(0) { it }) {
+        writer.println("Hello")
+        SystemClock.sleep(1000)
+    }
+
+    socket.close()
+
 }
