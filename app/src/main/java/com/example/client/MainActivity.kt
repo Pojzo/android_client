306b5e2f5b05cbefe18917a525240c63cc4bcd23
Peter Kováč <pojzinko8@gmail.com>
2023-03-16 23:31:57 +0100

    Working asynchronously (I guess, I'll have to do async later)

--- app/src/main/java/com/example/client/MainActivity.kt
+++ app/src/main/java/com/example/client/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.example.client
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.SystemClock
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.Socket
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +17,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sendMsgButton: Button
     private lateinit var sendMsgInput: TextInputEditText
     private lateinit var recvMsgText: TextView
+    private lateinit var connectionStatusText: TextView
+    private lateinit var connectBtn: Button
+
+    private val lock = Object()
+    private val destinationAddress = "192.168.100.7"
+    private val port = 12345
+
+    private lateinit var socket: Socket
+    private lateinit var socketWriter: PrintWriter
+    private lateinit var socketReader: BufferedReader
+
+    private var msg_out_ready: Boolean = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,29 +37,54 @@ class MainActivity : AppCompatActivity() {
         sendMsgButton = findViewById(R.id.send_msg_btn)
         sendMsgInput = findViewById(R.id.send_msg_input)
         recvMsgText = findViewById(R.id.recv_msg_text)
+        connectionStatusText = findViewById(R.id.connection_status_text)
+        connectBtn = findViewById(R.id.connect_btn)
 
+        connectBtn.setOnClickListener {
+            connect()
+        }
+
+        sendMsgButton.setOnClickListener {
+            sendButtonClicked()
+        }
+    }
+
+    private fun sendButtonClicked() {
+        val clientMsg = sendMsgInput.text
         Thread {
-            try {
-                createSocketConnection()
-            } catch (e: Exception) {
-                Log.e("SocketError", "Error occurred: ${e.message}")
-            }
+            socketWriter.println(clientMsg)
         }.start()
     }
-}
 
-private fun createSocketConnection() {
-    val destinationAddress = "192.168.100.7"
-    val port = 12345
-    val socket = Socket(destinationAddress, port)
-    val writer = PrintWriter(socket.getOutputStream(), true)
-    val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+    @SuppressLint("SetTextI18n")
+    private fun connect() {
+        val thread = Thread {
+            socket = Socket(destinationAddress, port)
+            if (!socket.isConnected()) {
+                return@Thread
+            }
+            val is_connected = socket.isConnected()
+            println("Should be connected: $is_connected")
+            socketWriter = PrintWriter(socket.getOutputStream(), true)
+            socketReader = BufferedReader(InputStreamReader(socket.getInputStream()))
+            connectionStatusText.text = "Connected to $destinationAddress"
 
-    for (i in generateSequence(0) { it }) {
-        writer.println("Hello")
-        SystemClock.sleep(1000)
+            runSocket()
+        }
+        thread.start()
     }
+    private fun runSocket() {
+        while (true) {
+            val serverMsg = socketReader.readLine()
+            recvMsgText.text = serverMsg
 
-    socket.close()
-
+            if (serverMsg == "BYE") {
+                socketWriter.println("BYE")
+                break
+            }
+        }
+        socketReader.close()
+        socket.close()
+    }
 }
+
