49b246a62496467670e16a4de781aa6025e012c5
Peter Kováč <pojzinko8@gmail.com>
2023-03-22 21:02:28 +0100

    New main UI (settings don't work)

--- app/src/main/java/com/example/client/MainActivity.kt
+++ app/src/main/java/com/example/client/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.client
 
 //noinspection SuspiciousImport
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -19,12 +21,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sendMsgButton: Button
     private lateinit var sendMsgInput: TextInputEditText
     private lateinit var recvMsgText: TextView
-    private lateinit var connectionStatusText: TextView
     private lateinit var connectBtn: Button
     private lateinit var settingsBtn: Button
+    private lateinit var ipStatusText: TextView
+    private lateinit var portStatusText: TextView
+    private lateinit var connectionStatusText: TextView
 
-    private val destinationAddress = "147.229.186.51"
-    private val port = 44444
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private var defaultIP: String = "0.0.0.0"
+    private var defaultPort: Int = 44444
 
     private lateinit var socket: Socket
     private lateinit var socketWriter: PrintWriter
@@ -35,12 +42,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         sendMsgButton = findViewById(R.id.send_msg_btn)
-        sendMsgInput = findViewById(R.id.send_msg_input)
-        recvMsgText = findViewById(R.id.recv_msg_text)
-        connectionStatusText = findViewById(R.id.connection_status_text)
-        connectBtn = findViewById(R.id.connect_btn)
-        settingsBtn = findViewById(R.id.settings_btn)
+        sendMsgInput = findViewById(R.id.send_msg_input_id)
+        recvMsgText = findViewById(R.id.recv_msg_text_id)
+        connectBtn = findViewById(R.id.connect_btn_id)
+        settingsBtn = findViewById(R.id.settings_btn_id)
+        connectionStatusText = findViewById(R.id.connection_status_text_id)
+        ipStatusText = findViewById(R.id.ip_status_text_id)
+        portStatusText = findViewById(R.id.port_status_text_id)
 
+        initSharedPreferences()
         setUpBtnListeners()
     }
 
@@ -76,17 +86,32 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     private fun connect() {
         val thread = Thread {
-            socket = Socket(destinationAddress, port)
+            val ip = sharedPreferences.getString("IP", "0.0.0.0")
+            val port = sharedPreferences.getInt("PORT", 44444)
+            socket = Socket(ip, port)
             if (!socket.isConnected) {
                 return@Thread
             }
             socketWriter = PrintWriter(socket.getOutputStream(), true)
             socketReader = BufferedReader(InputStreamReader(socket.getInputStream()))
-            connectionStatusText.text = "Connected to $destinationAddress"
+            connectionStatusText.text = "Connected to $ip"
 
             runSocket()
         }
         thread.start()
+    }
+
+    private fun initSharedPreferences() {
+        sharedPreferences = getSharedPreferences("NetworkSettings", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("IP", defaultIP)
+        editor.putInt("PORT", defaultPort)
+        editor.apply()
+
+        val ipString = "IP: ".plus(defaultIP)
+        val portString = "PORT :".plus(defaultPort.toString())
+        ipStatusText.text = ipString
+        portStatusText.text = portString
     }
     private fun runSocket() {
         while (true) {
@@ -101,5 +126,10 @@ class MainActivity : AppCompatActivity() {
         socketReader.close()
         socket.close()
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
 }
+
 
--- app/src/main/java/com/example/client/SettingsActivity.kt
+++ app/src/main/java/com/example/client/SettingsActivity.kt
@@ -1,39 +1,52 @@
 package com.example.client
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var ipInput: EditText
+    private lateinit var portInput: EditText
     private lateinit var saveChangesBtn: Button
     private lateinit var backBtn: Button
 
+    private var sharedPreference = getSharedPreferences("NetworkSettings", Context.MODE_PRIVATE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-
         setContentView(R.layout.settings_activity)
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        saveChangesBtn = findViewById(R.id.save_changes_btn)
 
+        ipInput = findViewById(R.id.settings_ip_input)
+        portInput = findViewById(R.id.settings_port_input)
+        saveChangesBtn = findViewById(R.id.settings_save_changes_btn)
         backBtn = findViewById(R.id.back_btn)
-        //toolbarBackBtn = findViewById(R.id.toolbar_back_btn)
+
+        ipInput.text = Editable.Factory.getInstance().newEditable(sharedPreference.getString("IP", "0.0.0.0"))
+        portInput.text = Editable.Factory.getInstance().newEditable(sharedPreference.getInt("PORT", 44444).toString())
+
+        saveChangesBtn.setOnClickListener {
+            updateSharedPreferences()
+        }
 
         backBtn.setOnClickListener {
             val intent = Intent(this@SettingsActivity, MainActivity::class.java)
             startActivity(intent)
         }
+    }
 
-        /*
-        toolbarBackBtn.setOnClickListener {
-            val intent = Intent(this@SettingsActivity, MainActivity::class.java)
-            startActivity(intent)
-        }
+    private fun updateSharedPreferences() {
+        val ip = ipInput.text.toString()
+        val port = portInput.text.toString().toInt()
 
-         */
-
+        val editor = sharedPreference.edit()
+        editor.putString("IP", ip)
+        editor.putInt("PORT", port)
+        editor.apply()
     }
 }
