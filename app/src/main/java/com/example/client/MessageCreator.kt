09f2963360a2ebd7714c13af897bb27f157942fc
Peter Kováč <pojzinko8@gmail.com>
2023-04-03 20:55:21 +0200

    Changed to one object

--- app/src/main/java/com/example/client/MessageCreator.kt
+++ app/src/main/java/com/example/client/MessageCreator.kt
@@ -2,30 +2,24 @@ package com.example.client
 
 import org.json.JSONObject
 
-open class MessageCreator {
-    open fun createUniversalMessage(type: String, content: String, timestamp: String): JSONObject {
+object MessageCreator {
+    private fun createUniversalMessage(type: String, content: String, timestamp: String): JSONObject {
         val message = JSONObject()
         message.put("type", type)
         message.put("content", content)
         message.put("timestamp", timestamp)
         return message
     }
-}
 
-class ClientMessageCreator : MessageCreator() {
-    fun createMessage(type: String, content: String, timestamp: String): String {
-        val message = super.createUniversalMessage(type, content, timestamp)
+    fun createClientMessage(type: String, content: String, timestamp: String): String {
+        val message = createUniversalMessage(type, content, timestamp)
         message.put("sender", "Client")
-
         return message.toString()
     }
-}
 
-class ServerMessageCreator : MessageCreator() {
-    fun createMessage(type: String, content: String, timestamp: String): String {
-        val message = super.createUniversalMessage(type, content, timestamp)
+    fun createServerMessage(type: String, content: String, timestamp: String): String {
+        val message = createUniversalMessage(type, content, timestamp)
         message.put("sender", "Server")
-
         return message.toString()
     }
 }
